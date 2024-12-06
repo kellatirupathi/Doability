33989b66851808707c12aba8b55410c10a98033f
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-12-07 01:49:55 +0530

    Update openai_api.py

--- utils/openai_api.py
+++ utils/openai_api.py
@@ -1,21 +1,29 @@
+import os
 import openai
+from openai import AzureOpenAI
 
-# Set the necessary configuration for Azure OpenAI
 openai.api_type = "azure"
-openai.api_key = "aa4f2f35c7634fcb8f5b652bbfb36926"
-openai.api_base = "https://nw-tech-dev.openai.azure.com/"
-openai.api_version = "2023-03-15-preview"
-
-# Set your deployment ID here
-deployment_id = "gpt-4o"  # Replace with your actual deployment ID
+client = AzureOpenAI(
+    # azure_endpoint="https://nw-tech-wu.openai.azure.com/",
+    # api_key="fce9b34907b848a6902e5c37ddfc8512",
+    # api_version="2024-02-01",
+    azure_endpoint="https://nw-tech-dev.openai.azure.com/",
+    api_key="aa4f2f35c7634fcb8f5b652bbfb36926",
+    api_version="2023-03-15-preview",
+)
 
 def analyze_text_with_openai(prompt):
     messages = [{"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": prompt}]
 
-    response = openai.ChatCompletion.create(
-        deployment_id=deployment_id,
+    response = client.chat.completions.create(
+        model="gpt-4o",
         messages=messages,
     )
 
-    return response.choices[0].message["content"].strip()
+    return response.choices[0].message.content.strip()
+
+# Example usage
+prompt = "Tell me about the benefits of using AI in business."
+result = analyze_text_with_openai(prompt)
+print(result)
